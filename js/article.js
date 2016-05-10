cb444ab7bbfc698a66724175d4f47a12de85f41c
OA Wu <comdan66@gmail.com>
2016-05-10 12:39:55 +0800

    Minify js、html, fix gh-pages path bug.

--- js/article.js
+++ js/article.js
@@ -1,10 +1,1 @@
-/**
- * @author      OA Wu <comdan66@gmail.com>
- * @copyright   Copyright (c) 2016 OA Wu Design
- */
-
-$(function () {
-  window.$body.find ('#_c ._c figure a').imgLiquid ({verticalAlign: 'center'});
-  window.$body.find ('#_c ._c article a').each (function () { $(this).attr ('target', '_blank'); });
-  window.$body.find ('#_c ._c .np figure > a').imgLiquid ({verticalAlign: 'center'});
-});
+$(function(){window.$body.find("#_c ._c figure a").imgLiquid({verticalAlign:"center"}),window.$body.find("#_c ._c article a").each(function(){$(this).attr("target","_blank")}),window.$body.find("#_c ._c .np figure > a").imgLiquid({verticalAlign:"center"})});
